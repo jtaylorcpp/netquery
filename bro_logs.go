dab4fc318991e4cf732de2d01be8fd949bdde413
jtaylorcpp <jtaylorcpp@gmail.com>
2019-01-02 08:38:30 -0500

    first commit of partially working agent

---
+++ bro_logs.go
@@ -0,0 +1,46 @@
+package netquery
+
+import (
+	"errors"
+	"fmt"
+	"log"
+
+	"github.com/hpcloud/tail"
+	"gitlab.com/nfgtech/broparser"
+)
+
+var supportedLogs = map[string]bool{
+	"conn.log": true,
+	"dns.log":  true,
+}
+
+func getBroTailer(path, logname string) (*tail.Tail, error) {
+	if _, ok := supportedLogs[logname]; !ok {
+		return nil, errors.New(fmt.Sprintf("BRO LOG NOT SUPPORTED: %s, dir: %s", logname, path))
+	}
+
+	switch logname {
+	case "conn.log":
+		return broparser.FollowConn(fmt.Sprintf("%s/%s", path, logname)), nil
+	case "dns.log":
+		return broparser.FollowDNS(fmt.Sprintf("%s/%s", path, logname)), nil
+	default:
+		return nil, errors.New("TAILER FAILED")
+	}
+}
+
+func GetBroTailers(path string, files []string) map[string]*tail.Tail {
+	tailMap := map[string]*tail.Tail{}
+
+	for _, file := range files {
+		tailer, err := getBroTailer(path, file)
+		if err != nil {
+			log.Printf("Error For file %s: %s", file, err.Error())
+		} else {
+			log.Printf("Watching file: %s/%s\n", path, file)
+			tailMap[file] = tailer
+		}
+	}
+
+	return tailMap
+}
---
+++ cmd/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+	"gitlab.com/nfgtech/netquery"
+)
+
+var broDir string
+var postgresHost string
+var postgresPort string
+var postgresDB string
+var postgresUser string
+var postgresPass string
+
+func init() {
+	runCmd.Flags().StringVarP(&broDir, "bro-dir", "b", "", "bro log directory to watch")
+	runCmd.Flags().StringVarP(&postgresHost, "psql-host", "", "localhost", "postgres hostname")
+	runCmd.Flags().StringVarP(&postgresPort, "psql-port", "", "5432", "postgres port")
+	runCmd.Flags().StringVarP(&postgresDB, "psql-db", "", "brodb", "postgres db name")
+	runCmd.Flags().StringVarP(&postgresUser, "psql-user", "", "brouser", "postgres username")
+	runCmd.Flags().StringVarP(&postgresPass, "psql-pass", "", "bropass", "postgres user pasword")
+
+	rootCmd.AddCommand(runCmd)
+}
+
+func main() {
+	execute()
+}
+
+func execute() {
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+var rootCmd = &cobra.Command{
+	Use:   "netquery",
+	Short: "netquery is a SQL service for network data",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("welcome to netquery! Use the -h flag to see more options.")
+	},
+}
+
+var runCmd = &cobra.Command{
+	Use:   "agent",
+	Short: "run netquery in agent mode",
+	Run: func(cmd *cobra.Command, args []string) {
+		log.Println("Starting the netquery agent.")
+		if broDir != "" {
+			broInfo, err := os.Stat(broDir)
+			if err != nil {
+				panic(err)
+			}
+
+			var path = ""
+			var files = []string{}
+
+			if !broInfo.IsDir() {
+				//log.Printf("Watching file: %v\n", broDir)
+				abspath, err := filepath.Abs(broDir)
+				if err != nil {
+					panic(err)
+				}
+				path = filepath.Dir(abspath)
+				files = append(files, filepath.Base(broDir))
+			} else {
+				//log.Printf("Watching directory: %v\n", broDir)
+				abspath, err := filepath.Abs(broDir)
+				if err != nil {
+					panic(err)
+				}
+				path = abspath
+				filesDir, err := ioutil.ReadDir(path)
+				if err != nil {
+					panic(err)
+				}
+
+				for _, f := range filesDir {
+					files = append(files, f.Name())
+				}
+			}
+
+			//log.Printf("path: %v\n", path)
+			//log.Printf("files: %v\n", files)
+			tailers := netquery.GetBroTailers(path, files)
+			log.Println(tailers)
+
+			netquery.StartServer(tailers)
+		}
+	},
+}
